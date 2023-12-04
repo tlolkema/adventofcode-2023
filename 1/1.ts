92d9e68752b9e8fb5e8cf24abdb2a8ce05861e62
Tim Lolkema <timlolkema@tims-air.kpn>
2023-12-04 23:44:48 +0100

    day one, refactor

--- 1/1.ts
+++ 1/1.ts
@@ -1,12 +1,11 @@
 const file = await Deno.readTextFile("./1/data.txt");
 
-const lines = file.trim().split("\n");
-
-let total = 0;
-
-lines.forEach((line) => {
-  const numbers = line.split("").filter((char) => !isNaN(parseInt(char)));
-  total += parseInt(numbers[0] + numbers[numbers.length - 1]);
-});
+const total = file
+  .trim()
+  .split("\n")
+  .reduce((acc, line) => {
+    const numbers = line.split("").filter((char) => Number(char));
+    return acc + Number(numbers[0] + numbers[numbers.length - 1]);
+  }, 0);
 
 console.log(total);
--- 1/2.ts
+++ 1/2.ts
@@ -1,26 +1,22 @@
 const file = await Deno.readTextFile("./1/data.txt");
 
-const lines = file.trim().split("\n");
+const total = file
+  .trim()
+  .split("\n")
+  .reduce((acc, line) => {
+    const modifiedLine = line
+      .replaceAll("one", "one1one")
+      .replaceAll("two", "two2two")
+      .replaceAll("three", "three3three")
+      .replaceAll("four", "four4four")
+      .replaceAll("five", "five5five")
+      .replaceAll("six", "six6six")
+      .replaceAll("seven", "seven7seven")
+      .replaceAll("eight", "eight8eight")
+      .replaceAll("nine", "nine9nine");
 
-let total = 0;
-
-lines.forEach((line) => {
-  const modifiedLine = line
-    .replaceAll("one", "one1one")
-    .replaceAll("two", "two2two")
-    .replaceAll("three", "three3three")
-    .replaceAll("four", "four4four")
-    .replaceAll("five", "five5five")
-    .replaceAll("six", "six6six")
-    .replaceAll("seven", "seven7seven")
-    .replaceAll("eight", "eight8eight")
-    .replaceAll("nine", "nine9nine");
-
-  const numbers = modifiedLine
-    .split("")
-    .filter((char) => !isNaN(parseInt(char)));
-
-  total += parseInt(numbers[0] + numbers[numbers.length - 1]);
-});
+    const numbers = modifiedLine.split("").filter((char) => Number(char));
+    return acc + Number(numbers[0] + numbers[numbers.length - 1]);
+  }, 0);
 
 console.log(total);
